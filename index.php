4510298fa162b3c53113aa412edecec90f5172db
lu-rios97 <danielriver492@gmail.com>
2022-10-21 06:49:59 -0500

    este es un commit

---
+++ index.php
@@ -0,0 +1,306 @@
+<?php include("vistas/cabecera.php"); ?>
+<br>
+<br>
+<br>
+
+<h1 style="text-align:center;" >Manual Tecnico </h1>
+<br>
+<br>
+<br>
+
+<div class="container">
+    <div class="row">
+
+        <div  class="col-6" style="border: solid 2px black; border-radius: 10px;">
+            <h4 style="background-color:bisque;" >Contenido</h4>
+            <br>
+        
+           <p> 1.	ALCANCE </p>
+           <p> 1.	1 Objetivo general </p>
+           <p> 1.	2 objetivos específicos </p>
+           <p> 2.	DESCRIPCION DE PROCESOS </p>
+           <p> 3.	DIAGRAMAS UML </p>
+           <p> 4.	DESCRIPCION DE LA PLATAFORMA </p>
+           <p> 4.   1 PHP </p>
+           <p> 4.   2 SERVIDOR DE BASE DE DATOS (MYSQL) </p>
+           <p> 4.   3 ANDROID </p>
+           <p> 5.	MODELO RELACIONAL DE LA BASE DE DATOS </p>
+           <p> 6.	DESCRIPCIÓN DE LOS ACUERDOS DE NIVELES DE SERVICIOS O ANS </p>
+
+        </div> 
+        
+        <div class="col-6" style="border: solid 2px black; border-radius: 10px; text-align:right;">
+            <h5 style="background-color:bisque; text-align:right;" >Pagina</h5>
+            <br>
+            <br>
+            <br>
+
+            <p> 1</p>
+            <p> 2</p>
+            <p> 3</p>
+            <p> 4</p>
+            <p> 5</p>
+            <p> 6</p>
+            <p> 7</p>
+            <p> 8</p>
+            <p> 9</p>
+            <p> 10</p>
+            <p> 11</p>   
+
+        </div>
+        
+    </div>
+</div>
+</div>
+        <br>
+        <br>
+        <br>
+
+
+    <div class="container" style="border: solid 2px black; border-radius: 10px;" > 
+        <div class="row">
+
+            <div class="col-6"> 
+                <h2 style="background-color:bisque;">Alcance</h2>
+                <br>
+                <h5 style="background-color:bisque;">Objetivo general:</h5>
+                <br>
+                Realizar un manual que sirva de guía para cumplir con las normas de salud y seguridad en el trabajo, para permitir así a nuestros clientes una mejor comunicación y una serie de capacitaciones sobre los riesgos y peligros a los que se encuentran los trabajadores en el tiempo que desempeñan sus labores.
+                </p>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <p style="text-align:right;" >pagina 1</p>
+
+            </div>
+            <br>
+            <br>
+            <br>
+
+            <div class="col-6">
+            <h5 style="background-color:bisque; text-align:right;" ></h5>
+            <br>
+            <br>
+            <br>
+
+        </div>
+
+
+        </div>
+
+
+    </div>
+    <br>
+    <br>
+    <br>
+
+    <div class="container" style="border: solid 2px black; border-radius: 10px;" > 
+        <div class="row">
+
+            <div class="col-6"> 
+                <h2 style="background-color:bisque;">Objetivo específicos:</h2>
+                <br>
+                <h5 style="background-color:bisque;"></h5>
+                
+                <p> ●	Definir procesos de manera ordenada. </p>
+                <p> ●	Determinar los procesos adecuados para que se puedan unir opiniones al momento de realizar las actividades.</p>
+                <p> ●	Establecer responsabilidades para que todas las actividades se desarrollen de manera coordinada.</p>
+                
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <p style="text-align:right;" >pagina 2</p>
+
+            </div>
+
+            </div>
+</div>
+<br>
+            <br>
+            <br>
+
+
+<div class="container" style="border: solid 2px black; border-radius: 10px;" > 
+        <div class="row">
+
+            <div class="col-6"> 
+                <h2 style="background-color:bisque;">Descripción de procesos:</h2>
+                <br>
+                <h5 style="background-color:bisque;"></h5>
+                
+                <p> El sistema para fomentar las guías y capacitaciones sobre la salud y seguridad en el trabajo soporta el caso de uso al Ingresar al Aplicativo de acuerdo al siguiente gráfico:            </p>
+                <img src="img/caso.png" alt="logo" style="width: 950px;">
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <p style="text-align:right;" >pagina 3</p>
+
+            </div>
+
+            </div>
+</div>
+<br>
+<br>
+<br>
+
+
+<div class="container" style="border: solid 2px black; border-radius: 10px;" > 
+        <div class="row">
+
+            <div class="col-6"> 
+                <h2 style="background-color:bisque;">Descripcion de la plataforma:</h2>
+                <br>
+                <h5 style="background-color:bisque;"></h5>
+                
+                <p>SISOS HELP aplicación web y móvil que se enfoca en ayudar a los sisos de una empresa y poder llevar un mejor orden en esta, en esta app se ingresarán los registros de los empleados que tengan un incidente, accidente o falla, los empleados podrán ingresar y encontrar su respectivo registro con información detallada y algunos consejos u recomendaciones par a este.  </p>
+                <p>La interfaz está desarrollada por medio del lenguaje HTML, PHP, CSS, JavaScript.  </p>
+                <p> Para tener acceso al sistema se podrá realizar vía web, utilizando navegadores web como Google Chrome y mediante cualquier dispositivo electrónico que cuente con acceso a Internet. </p>
+                <p>Para instalar el servidor local de la aplicación se requiere lo siguiente: </p>
+                <p>- El sistema puede funcionar con cualquier sistema operativo. </p>
+                <p>- La App debe ser ejecutable desde un Smartphone con sistema Android o IOS. </p>
+                <p>- Computadores con mínimo 1gb de RAM y una memoria interna con mi mínimo 256 GB. </p>
+
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <p style="text-align:right;" >pagina 4</p>
+
+            </div>
+
+            </div>
+</div>
+<br>
+<br>
+<br>
+
+<div class="container" style="border: solid 2px black; border-radius: 10px;" > 
+        <div class="row">
+
+            <div class="col-6"> 
+                <h2 style="background-color:bisque;">Modelo relacional de la base de datos:</h2>
+                <br>
+                <h5 style="background-color:bisque;">El modelo relacional de la base de datos se muestra a continuación:</h5>
+               
+                <img src="img/modelo.png" alt="logo" style="width: 950px;">
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <p style="text-align:right;" >pagina 5</p>
+
+            </div>
+
+            </div>
+</div>
+<br>
+<br>
+<br>
+
+<div class="container" style="border: solid 2px black; border-radius: 10px;" > 
+        <div class="row">
+
+            <div class="col-6"> 
+                <h2 style="background-color:bisque;">Descripcion de proyecto:</h2>
+                <br>
+                <h5 style="background-color:bisque;"></h5>
+                
+                <img src="img/desc.png" alt="logo" style="width: 950px;">
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <p style="text-align:right;" >pagina 6</p>
+
+            </div>
+
+            </div>
+</div>
+<br>
+<br>
+<br>
+
+<div class="container" style="border: solid 2px black; border-radius: 10px;" > 
+        <div class="row">
+
+            <div class="col-6"> 
+                <h2 style="background-color:bisque;">Documentación del proyecto:</h2>
+                <br>
+                <h5 style="background-color:bisque;"></h5>
+                
+                <p>Controlador: Contiene la clase Controlador. </p>
+                <p>Modelo: Contiene las clases que están asociadas con la base de datos. </p>
+                <p>Vista: Contiene los directorios que implementan la vista de la aplicación. </p>
+                <p>Vista / CSS: Contiene la definición de los archivos CSS o estilos en cascada. </p>
+                <p>Vista / HTML: Contiene los formularios de la aplicación web.  </p>
+                <p>Vista / imagen: Contiene las imágenes usadas en la aplicación. </p>
+                <p>Vista / Jquery: Contiene la librería Jquery y Jquery UI. </p>
+                <p>Index.php: Es el arrancador de la aplicación. </p>
+
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <p style="text-align:right;" >pagina 6</p>
+
+            </div>
+
+            </div>
+</div>
+<br>
+<br>
+<br>    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+  
+        
+
+<?php include("vistas/pie.php"); ?>
