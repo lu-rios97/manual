e1eab92e2204acdd3403a0eda072956013148aeb
lu-rios97 <danielriver492@gmail.com>
2022-10-21 06:54:34 -0500

    es un commit

---
+++ vistas/cabecera.php
@@ -0,0 +1,52 @@
+
+<html lang="en">
+<head>
+  <title>Manual Tecnico HTML y CCS3</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="stilo.css">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+  <!-- Brand/logo -->
+  <a class="navbar-brand" href="#">
+    <img src="img/001.png" alt="logo" style="width:85px;">
+  </a>
+  
+  <!-- Links -->
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#"> </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#"> </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#"> </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#"> </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#"> </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#"> </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#"></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#"> </a>
+    </li>
+  </ul>
+</nav>
+
+<div class="container-fluid">
+  
+
